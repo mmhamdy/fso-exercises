7b009bfc778e0e8d3f7ee3cb7c6e9fa07c2b0567
mmhamdy <mohamdy2772@gmail.com>
2022-01-24 11:28:34 +0200

    refactoring App into Header, Content, and Total components

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,5 +1,33 @@
 import React from 'react'
 
+const Header = ({course}) => {
+  return(
+    <h1>{course}</h1>
+  )
+}
+
+const Content = (props) => {
+  return(
+    <>
+      <p>
+        {props.part1} {props.exercises1}
+      </p>
+      <p>
+        {props.part2} {props.exercises2}
+      </p>
+      <p>
+        {props.part3} {props.exercises3}
+      </p>
+    </>
+  )
+}
+
+const Total = ({exercises1, exercises2, exercises3}) => {
+  return(
+    <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+  )
+}
+
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
@@ -11,17 +39,15 @@ const App = () => {
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course={course} />
+      <Content part1={part1}
+               exercises1={exercises1}
+               part2={part2}
+               exercises2={exercises2}
+               part3={part3}
+               exercises3={exercises3}
+      />
+      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
     </div>
   )
 }
