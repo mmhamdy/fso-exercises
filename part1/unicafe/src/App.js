844cf98b2add15224446f5b91cbc6f4d49d67c18
mmhamdy <mohamdy2772@gmail.com>
2022-01-26 14:56:54 +0200

    fix: use tbody tag to wrap the table rows

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -17,12 +17,14 @@ const Statistics = ({good, neutral, bad}) => {
       <div>
         <h1>Statistics</h1>
         <table>
+          <tbody>
             <StatisticLine text='good' value={good} />
             <StatisticLine text='neutral' value={neutral} />
             <StatisticLine text='bad' value={bad} />
             <StatisticLine text='all' value={good + neutral + bad} />
             <StatisticLine text='average' value={(good - bad)/(good + neutral + bad)} />
             <StatisticLine text='positive' value={(good / (good + neutral + bad)) * 100 + '%'} />
+          </tbody>
         </table>
       </div>
     )
