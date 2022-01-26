13774325812917db4d6879da48f536945855f017
mmhamdy <mohamdy2772@gmail.com>
2022-01-26 14:51:50 +0200

    display statistics in HTML table

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -2,19 +2,28 @@ import React, { useState } from 'react'
 
 const Button = ({text, onClick}) => <button onClick={onClick}>{text}</button>
 
-const StatisticLine = ({text, value}) => <p>{text} {value}</p>
+const StatisticLine = ({text, value}) => {
+  return (
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
+  )
+}
   
 const Statistics = ({good, neutral, bad}) => {
   if (good || neutral || bad) {
     return (
       <div>
         <h1>Statistics</h1>
-        <StatisticLine text='good' value={good} />
-        <StatisticLine text='neutral' value={neutral} />
-        <StatisticLine text='bad' value={bad} />
-        <StatisticLine text='all' value={good + neutral + bad} />
-        <StatisticLine text='average' value={(good - bad)/(good + neutral + bad)} />
-        <StatisticLine text='positive' value={(good / (good + neutral + bad)) * 100 + '%'} />
+        <table>
+            <StatisticLine text='good' value={good} />
+            <StatisticLine text='neutral' value={neutral} />
+            <StatisticLine text='bad' value={bad} />
+            <StatisticLine text='all' value={good + neutral + bad} />
+            <StatisticLine text='average' value={(good - bad)/(good + neutral + bad)} />
+            <StatisticLine text='positive' value={(good / (good + neutral + bad)) * 100 + '%'} />
+        </table>
       </div>
     )
   }
